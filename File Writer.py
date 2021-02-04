cc41fe56f9480e3a708f516a4e6baa5deb55ceba
D3nys2007 <74767164+D3nys2007@users.noreply.github.com>
2021-02-04 13:49:06 +0000

    Update File Writer.py

--- File Writer.py
+++ File Writer.py
@@ -1,6 +1,9 @@
-f = open('Reatrd.txt', 'w')
-
+textfile = input('Enter The File Name Make Sure To Put same name as in the spam bot txt file:\n' + '.txt')
+ 
+                 
+f = open(textfile, 'w')
+text = input('What Do You Want To File Your File With?\n:' + \n)
 for i in range(100000):
-    f.write('Durze is God\n')
+    f.write(text)
     
-
+# If There Are ANy Issues Please Contact me or fix it if you can
